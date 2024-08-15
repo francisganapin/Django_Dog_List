9d6b41ceaa8f45e45267b0127589d553b4ce8595
francis Valenzuela Ganapin <66492676+francisganapin@users.noreply.github.com>
2024-08-15 16:35:47 +0800

    Commit on 8/15/2024

--- mypage/urls.py
+++ mypage/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import dog_views
+from .views import dog_views,about_views
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
@@ -7,7 +7,9 @@ from django.conf import settings
 
 
 urlpatterns =[
-    path('',dog_views,name='dog_views'),
+    
+    path('dog',dog_views,name='dog_views'),
+    path('about',about_views,name='about_views'),
     path('dog/<str:breed_name>/',views.dog_detail,name='dog_detail'),
 ]
 if settings.DEBUG:
--- mypage/views.py
+++ mypage/views.py
@@ -14,3 +14,6 @@ def dog_detail(request,breed_name):
         return render(request,'dog_not_found.html')
     
     return render(request,'dog_detail.html',{'dog':dog})
+
+def about_views(request):
+    return render(request,'about.html')
