cf41a5ff09e0340141b3bec68fb843c4dd40235a
francis Valenzuela Ganapin <66492676+francisganapin@users.noreply.github.com>
2025-01-19 04:22:13 +0800

    1/19/2025

--- mypage/views.py
+++ mypage/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from .models import Dog
-
+from django.core.paginator import Paginator
 
 def dog_views(request):
     dogs = Dog.objects.all()
-    return render(request,'index.html',{'dogs':dogs})
+
+    
+    paginator = Paginator(dogs,10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {'dogs':dogs,'page_obj':page_obj}
+
+    return render(request,'index.html',context)
 
 
 def dog_detail(request,breed_name):
